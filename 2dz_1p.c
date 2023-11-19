f0fd3818d26ed8ca46dd6655fcd6508749001972
Buntar123 <123300385+Buntar123@users.noreply.github.com>
2023-11-19 16:24:26 +0700

    Add files via upload

---
+++ 2dz_1p.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define N 3
+int main(void){
+	int A[N][N];
+	int m=1;
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+		A[j][i]=m;
+		m++;
+	    }
+	}
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			printf("%d",A[j][i]);
+			printf(" ");
+	    }
+	printf("\n");
+	}
+	return 0; 
+}
